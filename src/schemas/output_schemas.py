35e82c4b6a65f9fd26ee03e5827fa4a157dc7f58
juanignaciosolis <juanignaciop.solis@gmail.com>
2026-07-26 15:40:12 -0300

    FEATURE: modifica los outputs schemas agregando un discriminador de 2 esquemas

--- src/schemas/output_schemas.py
+++ src/schemas/output_schemas.py
@@ -6,17 +6,37 @@ logger: Logger = setup_logger(name=__name__)
 
 from pathlib import Path
 from pydantic import BaseModel, Field
+from typing import Literal
 
 
-class AnswerScheme(BaseModel):
-    reasonly: str = Field(description="Descripción corta de los pasos razonados para obtener la respuesta",
-                          max_length= 300)
+class SQLAnswer(BaseModel):
+    type: Literal["sql_success"] = Field(default="sql_success",
+        description="Identificador para respuestas SQL válidas")
     query: str = Field(description="Consulta SQL que responde la pregunta del usuario",
                        max_length= 500)
-    human_revision: bool = Field(description="Establecer en True cuando la pregunta sea ambigua, no hay suficientes datos para responderlas o se detecte una solicitud no correspondida")
-
+    human_revision: bool = Field(description="Establecer en True cuando la pregunta sea ambigua, no hay suficientes datos para responderla")
     confidence: float = Field(
         ge=0.0,
         le=1.0,
         description="Confianza estimada entre 0 y 1."
-    )    
+    ) 
+class InvalidAnswerScheme(BaseModel):
+    type: Literal["invalid_query"] = Field(
+        default="invalid_query",
+        description="Identificador para respuestas donde no se pudo generar la consulta")
+    error: str = Field(description="Titulo del error, problema o inconveniente")
+    resumen: str = Field(description="Descripcion sencilla y corta del error, problema o incoveniente",
+                         max_length= 300)
+    evidence: list[str] = Field(description="Citar de la pregunta del usuario evidencias del problema",
+                                min_length=1, max_length=3)   
+    confidence: float = Field(
+        ge=0.0,
+        le=1.0,
+        description="Confianza estimada entre 0 y 1."
+    ) 
+
+class AnswerWrapper(BaseModel):
+    payload: SQLAnswer | InvalidAnswerScheme = Field(
+        discriminator="type",
+        description="Resultado del procesamiento: la consulta generada o el error detallado"
+    )
